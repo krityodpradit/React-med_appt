b8e42630914640840779f46421b613caf44ff756
Krit <krityodpradit@hotmail.com>
2023-11-05 00:59:13 -0400

    add built and seo

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_URL = window.location.hostname === "localhost" ? "https://krityodpradi-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://krityodpradi-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
 console.log(
-    "API_URL :",
+    "API_URL : window.location.origin",
     API_URL
 );
